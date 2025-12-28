c22170e27c34980785168c25145c58c9371fcabd
Annan-Andrews <annanandrews03@gmail.com>
2025-12-29 01:09:52 +0530

    feat: integrate Redux for state management and add user authentication slice

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router";
-
+import { CartProvider } from "./context/CartContext";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
-  return <RouterProvider router={router} />
+  return (
+    <Provider store={store}>
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ client/src/redux/userSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// User Slice
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    isUserAuth: false,
+    userData: {},
+  },
+  reducers: {
+    setUserAuth: (state) => {
+      state.isUserAuth = true;
+    },
+    saveUser: (state, action) => {
+      state.isUserAuth = true;
+      state.userData = action.payload;
+    },
+    clearUser: (state) => {
+      state.isUserAuth = false;
+      state.userData = {};
+    },
+  },
+});
+
+// Export actions
+export const { setUserAuth, saveUser, clearUser } = userSlice.actions;
+
+// Export reducers
+export default userSlice.reducer;
